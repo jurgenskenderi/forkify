c85a340a6b0a2f46fd9daf43597bdb74178aa7ef
jurgenskenderi <jurgenskenderi20@gmail.com>
2022-05-03 16:12:33 -0400

    Initial commit

---
+++ finalApp/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'a2e2fec0-4c46-49f0-925e-766707791dbb';
+export const MODAL_CLOSE_SEC = 2.5;
